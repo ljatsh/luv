430af42b6c3fa8ea7b2201d7bdcbcfb5b28ad0b6
lj@sh <longjun.wang@qq.com>
2018-09-10 21:02:57 +0800

    added libuv

--- gen_mac.sh
+++ gen_mac.sh
@@ -1,4 +1,8 @@
 
 gyp -f xcode --generator-output=build --depth=. lua/lua.gyp
 
-xcodebuild -ARCHS="x86_64" -project build/lua/lua.xcodeproj -configuration Release -target All
+#xcodebuild -ARCHS="x86_64" -project build/lua/lua.xcodeproj -configuration Release -target All
+
+gyp -f xcode --generator-output=build --depth=. -Duv_library=static_library  -Ilibuv/common.gypi  libuv/uv.gyp
+
+xcodebuild -ARCHS="x86_64" -project build/libuv/uv.xcodeproj -configuration Release -target All
